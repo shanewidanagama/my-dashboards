e26fad67e1154abb503142d50ca7083760bad2ce
Shane Widanagama <46659817+shanewidanagama@users.noreply.github.com>
2024-12-03 21:09:24 -0500

    Rename links page title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Useful Links</h1>
+        <h1>Important Links</h1>
         <ol>
           <li>
             <a href="https://www.mortgagesandbox.com/">mortgagesandbox.com</a>
